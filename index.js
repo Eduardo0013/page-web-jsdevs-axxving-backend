462112bd3b974a41484900e0e65cca656fd7e862
Eduardo0013 <thewarrior20013@gmail.com>
2024-06-01 23:09:21 -0600

    feat(middlewares): se añadio un middleware para manejar rutas no encontradas

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const app = Express()
 
 app.use(bodyParser.json())
 app.use('/',require('./routes/contacto.js'))
-
+app.use((req,res,next) => {
+    res.status(404).send('Not Found')
+})
 const server = app.listen(process.env.PORT ?? 8080)
 
 module.exports = {
